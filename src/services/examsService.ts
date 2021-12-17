10e98ac3cfbfa41203eeca06eed3da703d6be619
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-17 05:06:01 -0300

    feat: check category before posting new exams

--- src/services/examsService.ts
+++ src/services/examsService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import CategoryEntity from '../entities/CategoryEntity';
 import ExamEntity from '../entities/ExamEntity';
 import TeacherDisciplineRelationEntity from '../entities/TeacherDisciplineRelationEntity';
 import NewExam from '../interfaces/NewExam';
@@ -10,6 +11,12 @@ async function postExam(newExam: NewExam) {
       discipline_id: newExam.disciplineId,
     });
   if (!relation.length) throw new Error('This teacher does not teach this discipline');
+
+  const category = await getRepository(CategoryEntity)
+    .find({
+      id: newExam.categoryId,
+    });
+  if (!category.length) throw new Error('This category does not exist');
 
   await getRepository(ExamEntity).create(newExam);
   await getRepository(ExamEntity).save(newExam);
