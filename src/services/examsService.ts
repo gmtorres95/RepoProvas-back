6eeb94206b89569e9ba228a37b106f5c9f37d7a1
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-17 05:02:23 -0300

    feat: check for teacher/discipline relation

--- src/services/examsService.ts
+++ src/services/examsService.ts
@@ -1,8 +1,16 @@
 import { getRepository } from 'typeorm';
 import ExamEntity from '../entities/ExamEntity';
+import TeacherDisciplineRelationEntity from '../entities/TeacherDisciplineRelationEntity';
 import NewExam from '../interfaces/NewExam';
 
 async function postExam(newExam: NewExam) {
+  const relation = await getRepository(TeacherDisciplineRelationEntity)
+    .find({
+      teacher_id: newExam.teacherId,
+      discipline_id: newExam.disciplineId,
+    });
+  if (!relation.length) throw new Error('This teacher does not teach this discipline');
+
   await getRepository(ExamEntity).create(newExam);
   await getRepository(ExamEntity).save(newExam);
 }
