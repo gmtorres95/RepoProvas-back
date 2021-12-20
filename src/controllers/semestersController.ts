2112fa61af89a9bef7a7be2a9b78575568d5fb59
Gabriel <al2012.163.torres@gmail.com>
2021-12-20 04:52:29 -0300

    feat: add custom error NoSemesterFound

--- src/controllers/semestersController.ts
+++ src/controllers/semestersController.ts
@@ -6,6 +6,7 @@ async function getSemester(req: Request, res: Response, next: NextFunction) {
     const semesters = await semestersService.getSemesters();
     res.send(semesters);
   } catch (error) {
+    if (error.name === 'NoSemesterFound') return res.status(404).send(error.message)
     next(error);
   }
 }
